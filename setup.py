5daab9a9ab9edf11d961e00dd65df8bedd1f3be6
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2013-08-09 09:34:39 +0200

    added executable script 'mopad' which calls 'python -m mopad ...'

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '0.9b',
     description = 'Moment tensor Plotting and Decomposition tool',
     py_modules = ['mopad'],
+    scripts = ['mopad'],
     author = 'Lars Krieger',
     author_email = 'lars.krieger@zmaw.de',
     url = 'http://geophysics.github.com/MoPaD/',
